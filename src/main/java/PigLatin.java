721ddb2f768ec9451728d6c0d36965437e581215
gorajski <nickgorajski@gmail.com>
2017-04-24 22:09:24 -0500

    Implements single-word conversion and basic sentence from English to Pig Latin.  No support for special characters or capitalization.

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,6 +1,6 @@
 public class PigLatin {
 
-    public static String convert(String input) {
+    public static String convertWord(String input) {
         String firstLetter = input.substring(0, 1);
         Integer firstVowelPosition = 0;
         if (isVowel(firstLetter)) {
@@ -14,7 +14,32 @@ public class PigLatin {
         return(input.substring(firstVowelPosition) + input.substring(0, firstVowelPosition) + "ay");
     }
 
+    public static String convertSentence(String input) {
+        Integer start = -1;
+        Integer end;
+        String output = "";
+        for(Integer letterPosition = 0; letterPosition < input.length(); letterPosition++) {
+
+            if (!isSpace(input.substring(letterPosition, letterPosition + 1)) && (start.equals(-1))) {
+                start = letterPosition;
+            } else if (isSpace(input.substring(letterPosition, letterPosition + 1))) {
+                end = letterPosition;
+                output += convertWord(input.substring(start, end)) + " ";
+                start = -1;
+            } else if (letterPosition == input.length() - 1) {
+                end = input.length();
+                output += convertWord(input.substring(start, end));
+            }
+
+        }
+        return output.substring(0, output.length());
+    }
+
     private static Boolean isVowel(String letter) {
         return letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u");
+    }
+
+    private static Boolean isSpace(String letter) {
+        return letter.equals(" ");
     }
 }
--- src/test/java/PigLatinTests.java
+++ src/test/java/PigLatinTests.java
@@ -7,61 +7,72 @@ public class PigLatinTests {
     @Test
     @DisplayName("it should return apple because it starts with a vowel")
     public void testInitialVowelWordApple() {
-        assertEquals("apple", PigLatin.convert("apple"));
+        assertEquals("apple", PigLatin.convertWord("apple"));
     }
 
     @Test
     @DisplayName("it should return evil because it starts with a vowel")
     public void testInitialVowelWordEvil() {
-        assertEquals("evil", PigLatin.convert("evil"));
+        assertEquals("evil", PigLatin.convertWord("evil"));
     }
 
     @Test
     @DisplayName("it should return idle because it starts with a vowel")
     public void testInitialVowelWordIdle() {
-        assertEquals("idle", PigLatin.convert("idle"));
+        assertEquals("idle", PigLatin.convertWord("idle"));
     }
 
     @Test
     @DisplayName("it should return oval because it starts with a vowel")
     public void testInitialVowelWordOval() {
-        assertEquals("oval", PigLatin.convert("oval"));
+        assertEquals("oval", PigLatin.convertWord("oval"));
     }
 
     @Test
     @DisplayName("it should return uvula because it starts with a vowel")
     public void testInitialVowelWordUvula() {
-        assertEquals("uvula", PigLatin.convert("uvula"));
+        assertEquals("uvula", PigLatin.convertWord("uvula"));
     }
 
     @Test
     @DisplayName("it should return eesbay because it starts with a consonant")
     public void testInitialConsonantWordBees() {
-        assertEquals("eesbay", PigLatin.convert("bees"));
+        assertEquals("eesbay", PigLatin.convertWord("bees"));
     }
 
     @Test
     @DisplayName("it should return eesechay because it starts with a consonant")
     public void testInitialConsonantWordCheese() {
-        assertEquals("eesechay", PigLatin.convert("cheese"));
+        assertEquals("eesechay", PigLatin.convertWord("cheese"));
     }
 
     @Test
     @DisplayName("it should return eezgay because it starts with a consonant")
     public void testInitialConsonantWordGeez() {
-        assertEquals("eezgay", PigLatin.convert("geez"));
+        assertEquals("eezgay", PigLatin.convertWord("geez"));
     }
 
     @Test
     @DisplayName("it should return easpay because it starts with a consonant")
     public void testInitialConsonantWordPeas() {
-        assertEquals("easpay", PigLatin.convert("peas"));
+        assertEquals("easpay", PigLatin.convertWord("peas"));
     }
 
     @Test
     @DisplayName("it should return easeplay because it starts with a consonant")
     public void testInitialConsonantWordPlease() {
-        assertEquals("easeplay", PigLatin.convert("please"));
+        assertEquals("easeplay", PigLatin.convertWord("please"));
     }
 
+    @Test
+    @DisplayName("it should return 'ourfay orescay and evensay earsyay ago'")
+    public void testSentenceFourScoreAndSevenYearsAgo() { assertEquals("ourfay orescay and evensay earsyay ago", PigLatin.convertSentence("four score and seven years ago")); }
+
+    @Test
+    @DisplayName("it should return 'appyhay irthdaybay otay ouyay'")
+    public void testSentenceHappyBirthdayToYou() { assertEquals("appyhay irthdaybay otay ouyay", PigLatin.convertSentence("happy birthday to you")); }
+
+    @Test
+    @DisplayName("it should return 'ed ate an apple'")
+    public void testSentenceEdAteAnApple() { assertEquals("ed ate an apple", PigLatin.convertSentence("ed ate an apple")); }
 }
